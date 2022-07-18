c6f8720a67dc9cdac1a6fbf36b52b02c954a45b9
Martin CABOTTE <cabottemartin1@gmail.com>
2022-07-18 08:16:54 +0000

    Analyse Critère non itératif

---
+++ analyseCritereNonIteratif.R
@@ -0,0 +1,436 @@
+#' @title Analyse des critères avec des algorithmes non itératif
+#' @author Martin CABOTTE
+#' @description 
+#' @param data the dataset.
+#' @param trueLabels true labels of the dataset.
+#' @param trueK If user wants to perform analysis using the trueK value.
+#' @param spectral specify is the analysis is in spectral embedded space or initial rought feature plan.
+#' @param iteration The number of iterations in order to compute mean value.
+#' @param algorithm the chosen algorithm : "kmean", "cmean", "ecmbel" for y.bel in ecm, "ecmpl" for y.pl in ecm
+#' @param filenameLabel filename of resulting clustering label computation
+#' @param filenameResults filename of resulting criteria computation
+#' @return returns a list containing the following elements:
+#' \itemize{
+#'  \item{Silhouette: }{min, max and mean value}
+#'  \item{ARI: }{min, max and mean value}
+#'  \item{Vmeasure: }{min, max and mean value}
+#'  \item{Folkes_Mallow index: }{min, max and mean value}
+#'  \item{F1 score: }{min, max and mean value}
+#'  \item{Accuracy: }{min, max and mean value}
+#'  \item{Precision: }{min, max and mean value}
+#'  \item{Recall: }{min, max and mean value}
+#'  \item{Overlap: }{min, max and mean value}
+#'  \item{Clusters: }{min, max, mean value as well as number of min and max values computed (i.e nbMax = 3 means that algorithms performed 3 times the max number of final clusters)}
+#' }
+
+analyseCritereNonIteratif <- function(data,trueLabels,spectral=FALSE,trueK=FALSE,
+                           iteration,
+                           algorithme,
+                           nstart=10,
+                           filenameLabel,filenameResults,
+                           beta=2, delta=10, alpha=1,
+                           verbose=FALSE, vois=3,
+                           flagDiagZero=FALSE,Kmax=20,tolerence = 0.99,threshold = 0.9)
+{
+  #Parameters
+  
+  avgSil = 0
+  minavgSil = 0
+  maxavgSil = 0
+  
+  ari = 0
+  minari = 0
+  maxari = 0
+  
+  Vmes = 0
+  minVmes = 0
+  maxVmes = 0
+  
+  FM = 0
+  minFM = 0
+  maxFM = 0
+  
+  F1 = 0
+  minF1 = 0
+  maxF1 = 0
+  
+  Precision = 0
+  minPrecision = 0
+  maxPrecision = 0
+  
+  Recall = 0
+  minRecall = 0
+  maxRecall = 0
+  
+  Accuracy = 0
+  minAccuracy = 0
+  maxAccuracy = 0
+  
+  Overlapvar = 0
+  minOverlapvar = 0
+  maxOverlapvar = 0
+  
+  nbclusters = 0
+  maxclusters = 0
+  minclusters = 100
+  cptm = 0
+  cptp = 0
+  loupe <- 0
+  
+  i<-1
+  
+  for(i in 1:iteration)
+  {
+    #On realise les differents algorithmes pour avoir les resultats
+    if(spectral == TRUE)
+    {
+        W <- NULL
+        W <- compute.similarity.ZP(data, vois=vois)
+      
+      if(nrow(data)<50){
+        W <- checking.gram.similarityMatrix(W, flagDiagZero = FALSE, 
+                                            verbose = verbose)
+      }else{
+        W <- checking.gram.similarityMatrix(W, flagDiagZero = flagDiagZero, 
+                                            verbose = verbose)
+      }
+      
+      W <- compute.laplacian.NJW(W, verbose = verbose)
+      
+      eigenValues <- W$eigen$values
+      if(verbose){print(eigenValues[1:10])}
+      
+      kClusters <- compute.kclust(eigenValues, method = "PEV", Kmax = Kmax, 
+                                  tolerence =  tolerence,threshold = threshold,
+                                  verbose = verbose)
+      
+      if(verbose){cat("K = ",kClusters,"\n")}
+      
+      if(kClusters<=1){kClusters=2}
+      
+      dataSpec=W$eigen$vectors[,1:kClusters]
+      dataSpec <- dataSpec/apply(dataSpec,MARGIN=1,FUN=function(x) norm(matrix(x),"f"))
+    }
+    else
+    {
+      dataSpec <- data
+      kClusters <- Kbrut(data,Kmax=Kmax,nstart=30,verbose=FALSE)
+    }
+    
+    if(trueK == TRUE)
+    {
+      kClusters <- max(trueLabels)
+    }
+    
+    
+    if(algorithme == "kmean"){result <- kmeans(dataSpec,kClusters,nstart=nstart,iter.max = 100)$cluster}
+    if(algorithme == "cmean")
+    {
+      clust <- cmeans(dataSpec, kClusters)
+      within <- clust$withinerror
+      if(verbose){message(within)}
+      groups <- clust$cluster
+      clus <- clust
+      for(l in 2:nstart)
+      {
+        clust <- cmeans(dataSpec, kClusters)
+        if(within > clust$withinerror)
+        {
+          within <- clust$withinerror
+          if(verbose){message(within)}
+          groups <- clust$cluster
+          clus <- clust
+        }
+      }
+      result <- groups
+    }
+    if(algorithme == "ecmbel")
+    {
+      clust <- ecm(dataSpec, kClusters, type="full", init="rand", beta=beta, delta=delta,alpha=alpha,ntrials=nstart,disp=FALSE)
+      result <- clust$y.bel
+    }
+    if(algorithme == "ecmpl")
+    {
+      clust <- ecm(dataSpec, kClusters, type="full", init="rand", beta=beta, delta=delta,alpha=alpha,ntrials=nstart,disp=FALSE)
+      result <- clust$y.pl
+    }
+    
+    
+    
+    if(i == 1)
+    {
+      write.csv(result,filenameLabel,row.names = FALSE)
+    }
+    else
+    {
+      previousLabels <- read.csv(filenameLabel)
+      unlink(filenameLabel)
+      Labels <- cbind(previousLabels, result)
+      write.csv(Labels,filenameLabel,row.names = FALSE)
+    }
+    
+    clus = length(unique(result))
+    nbclusters = nbclusters + clus
+    
+    if(maxclusters < clus)
+    {
+      maxclusters <- clus
+      cptp <- 0
+    }
+    if(minclusters > clus)
+    {
+      minclusters <- clus
+      cptm <- 0
+    }
+    if(minclusters == clus)
+    {
+      cptm <- cptm + 1
+    }
+    if(maxclusters == clus)
+    {
+      cptp <- cptp + 1
+    }
+    
+    totalLabel <- unique(as.factor(result))
+    index <- c(1:length(totalLabel))
+    array <- data.frame("label"= index,"algo"= totalLabel)
+    label <- vector(mode="numeric", length = length(data[,1]))
+    for(j in 1:length(data[,1]))
+    {
+      label[j] <- array$label[which(result[j] == array$algo)]
+    }
+    
+    # message(label)
+    
+    if(max(unique(label)) != 1)
+    {
+      if(verbose){message("---- Calcul des statistiques ----")}
+      if(verbose){message("---- Calcul de la silhouette moyenne ----")}
+      sil <- cluster::silhouette(as.numeric(factor(result)),dist(data))
+      recap <- summary(sil)
+      avgSil = avgSil + recap$avg.width
+      
+      if(i == 1)
+      {
+        minavgSil <- recap$avg.width
+        maxavgSil <- recap$avg.width
+      }
+      else
+      {
+        if(minavgSil>recap$avg.width)
+        {
+          minavgSil <- recap$avg.width
+        }
+        if(maxavgSil<recap$avg.width)
+        {
+          maxavgSil <- recap$avg.width
+        }
+      }
+      
+      if(verbose){message("---- Calcul de l'ARI ----")}
+      currentAri <- adj.rand.index(trueLabels,as.numeric(factor(result)))
+      ari = ari + currentAri
+      
+      if(i == 1)
+      {
+        minari <- currentAri
+        maxari <- currentAri
+      }
+      else
+      {
+        if(minari>currentAri)
+        {
+          minari <- currentAri
+        }
+        if(maxari<currentAri)
+        {
+          maxari <- currentAri
+        }
+      }
+      
+      if(verbose){message("---- Calcul de la Vmeasure ----")}
+      currentVmes <- v_measure(trueLabels, as.numeric(factor(result)), beta = 1)
+      Vmes = Vmes + currentVmes
+      
+      if(i == 1)
+      {
+        minVmes <- currentVmes
+        maxVmes <- currentVmes
+      }
+      else
+      {
+        if(minVmes>currentVmes)
+        {
+          minVmes <- currentVmes
+        }
+        if(maxVmes<currentVmes)
+        {
+          maxVmes <- currentVmes
+        }
+      }
+      
+      if(verbose){message("---- Calcul de la precision, recall, F1, Folkes-Mallow ----")}
+      
+      assign <- assignVote(trueLabels,as.integer(factor(result)))
+      
+      if(length(unique(factor(assign))) != length(unique(factor(trueLabels))))
+      {
+        manquant <- unique(trueLabels[!trueLabels %in% assign])
+        cpt <- 1
+        for(i in manquant)
+        {
+          assign[cpt] <- i
+          cpt <- cpt+1
+        }
+      }
+      
+      confusion <- confusionMatrix(factor(assign),factor(trueLabels))
+      
+      precisionVector <- confusion$byClass[,5]
+      recallVector <- confusion$byClass[,6]
+      F1Vector <- confusion$byClass[,7]
+      
+      precisionVector[is.na(precisionVector)] <- 0
+      recallVector[is.na(recallVector)] <- 0
+      F1Vector[is.na(F1Vector)] <- 0
+      currentFM <- sqrt(mean(recallVector)*mean(precisionVector))
+      
+      Precision = Precision + mean(precisionVector)
+      Recall = Recall + mean(recallVector)
+      F1 = F1 + mean(F1Vector)
+      FM = FM + currentFM
+      
+      if(i == 1)
+      {
+        minPrecision <- mean(precisionVector)
+        minRecall <- mean(recallVector)
+        minF1 <- mean(F1Vector)
+        maxPrecision <- mean(precisionVector)
+        maxRecall <- mean(recallVector)
+        maxF1 <- mean(F1Vector)
+        minFM <- currentFM
+        maxFM <- currentFM
+      }
+      else
+      {
+        if(minPrecision>mean(precisionVector))
+        {
+          minPrecision <- mean(precisionVector)
+        }
+        if(maxPrecision<mean(precisionVector))
+        {
+          maxPrecision <- mean(precisionVector)
+        }
+        if(minRecall>mean(recallVector))
+        {
+          minRecall <- mean(recallVector)
+        }
+        if(maxRecall<mean(recallVector))
+        {
+          maxRecall <- mean(recallVector)
+        }
+        if(minF1>mean(F1Vector))
+        {
+          minF1 <- mean(F1Vector)
+        }
+        if(maxF1<mean(F1Vector))
+        {
+          maxF1 <- mean(F1Vector)
+        }
+        if(minFM>currentFM)
+        {
+          minFM <- currentFM
+        }
+        if(maxFM<currentFM)
+        {
+          maxFM <- currentFM
+        }
+      }
+      
+      if(verbose){message("---- Calcul de l'Accuracy ----")}
+      currentAccuracy <- unname(confusion$overall[1])
+      Accuracy = Accuracy + currentAccuracy
+      
+      if(i == 1)
+      {
+        minAccuracy <- currentAccuracy
+        maxAccuracy <- currentAccuracy
+      }
+      else
+      {
+        if(minAccuracy>currentAccuracy)
+        {
+          minAccuracy <- currentAccuracy
+        }
+        if(maxAccuracy<currentAccuracy)
+        {
+          maxAccuracy <- currentAccuracy
+        }
+      }
+      
+      if(verbose){message("---- Calcul de l'overlap ----")}
+      currentOverlapvar <- Overlap(trueLabels,as.integer(factor(result)))
+      Overlapvar = Overlapvar + currentOverlapvar
+      
+      if(i == 1)
+      {
+        minOverlapvar <- currentOverlapvar
+        maxOverlapvar <- currentOverlapvar
+      }
+      else
+      {
+        if(minOverlapvar>currentOverlapvar)
+        {
+          minOverlapvar <- currentOverlapvar
+        }
+        if(maxOverlapvar<currentOverlapvar)
+        {
+          maxOverlapvar <- currentOverlapvar
+        }
+      }
+    }
+    else
+    {
+      loupe <- loupe+1
+      nbclusters <- nbclusters - 1
+    }
+    
+  }
+  
+  iteration <- iteration - loupe
+  
+  Final <- list("avgSil_Mean"=rbind(previousLabels$avgSil_Mean,avgSil/iteration),
+                "avgSil_Min"=rbind(previousLabels$avgSil_Min,minavgSil/1),
+                "avgSil_Max"=rbind(previousLabels$avgSil_Max,maxavgSil/1),
+                "ari_Mean"=rbind(previousLabels$ari_Mean,ari/iteration),
+                "ari_Min"=rbind(previousLabels$ari_Min,minari/1),
+                "ari_Max"=rbind(previousLabels$ari_Max,maxari/1),
+                "Vmes_Mean"=rbind(previousLabels$Vmes_Mean,Vmes/iteration),
+                "Vmes_Min"=rbind(previousLabels$Vmes_Min,minVmes/1),
+                "Vmes_Max"=rbind(previousLabels$Vmes_Max,maxVmes/1),
+                "FM_Mean"=rbind(previousLabels$FM_Mean,FM/iteration),
+                "FM_Min"=rbind(previousLabels$FM_Min,minFM/1),
+                "FM_Max"=rbind(previousLabels$FM_Max,maxFM/1),
+                "F1_Mean"=rbind(previousLabels$F1_Mean,F1/iteration),
+                "F1_Min"=rbind(previousLabels$F1_Min,minF1/1),
+                "F1_Max"=rbind(previousLabels$F1_Max,maxF1/1),
+                "Accuracy_Mean"=rbind(previousLabels$Accuracy_Mean,Accuracy/iteration),
+                "Accuracy_Min"=rbind(previousLabels$Accuracy_Min,minAccuracy/1),
+                "Accuracy_Max"=rbind(previousLabels$Accuracy_Max,maxAccuracy/1),
+                "Precision_Mean"=rbind(previousLabels$Precision_Mean,Precision/iteration),
+                "Precision_Min"=rbind(previousLabels$Precision_Min,minPrecision/1),
+                "Precision_Max"=rbind(previousLabels$Precision_Max,maxPrecision/1),
+                "Recall_Mean"=rbind(previousLabels$Recall_Mean,Recall/iteration),
+                "Recall_Min"=rbind(previousLabels$Recall_Min,minRecall/1),
+                "Recall_Max"=rbind(previousLabels$Recall_Max,maxRecall/1),
+                "Overlap_Mean"=rbind(previousLabels$Overlap_Mean,Overlapvar/iteration),
+                "Overlap_Min"=rbind(previousLabels$Overlap_Min,minOverlapvar/1),
+                "Overlap_Max"=rbind(previousLabels$Overlap_Max,maxOverlapvar/1),
+                "Cluster_nbclusters"=rbind(previousLabels$Cluster_nbclusters,nbclusters/iteration),
+                "Cluster_MaxCluster"=rbind(previousLabels$Cluster_MaxCluster,maxclusters/1),
+                "Cluster_MinCluster"=rbind(previousLabels$Cluster_MinCluster,minclusters/1),
+                "Cluster_nbMax"=rbind(previousLabels$Cluster_nbMax,cptp/1),
+                "Cluster_nbMin"=rbind(previousLabels$Cluster_nbMin,cptm/1))
+  
+  saveRDS(Final,filenameResults)
+}
